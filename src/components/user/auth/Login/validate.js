97d73e5538c6e9bc04c6865a1858018b53e3663e
imaximciuc <maximciuc.iurii@gmail.com>
2020-08-11 23:18:19 +0300

    username - email validation

--- src/components/user/auth/Login/validate.js
+++ src/components/user/auth/Login/validate.js
@@ -2,6 +2,8 @@ const validate = values => {
     const errors = {};
     if (!values.username) {
         errors.username = 'Required field'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.username)) {
+        errors.username = 'Invalid email address'
     }
 
     if (!values.password) {
