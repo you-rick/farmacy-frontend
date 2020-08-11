9bf6ecd94e49c0690ba301d1c7c2f9b1bf02521b
imaximciuc <maximciuc.iurii@gmail.com>
2020-08-11 21:22:08 +0300

    environment variables

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import * as axios from 'axios';
 import {authHeaders} from "../utils/helpers/auth-headers";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://qws0yjnzh8.execute-api.us-east-2.amazonaws.com/dev/'
+    baseURL: process.env.REACT_APP_SERVER_URL
 });
 
 
