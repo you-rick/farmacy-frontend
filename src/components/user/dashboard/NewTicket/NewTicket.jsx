81b9b67cd64d7e9baea45d7fee60353cf0f6331f
imaximciuc <maximciuc.iurii@gmail.com>
2020-08-20 18:32:22 +0300

    Add TinyMCE Editor to New Ticket page and Ticket Info popup + fix links in Leftbar

--- src/components/user/dashboard/NewTicket/NewTicket.jsx
+++ src/components/user/dashboard/NewTicket/NewTicket.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, Container } from '@material-ui/core';
+import { Editor } from '@tinymce/tinymce-react';
 import { LOCALE } from '../../../../locale';
 
 const NewTicket = () => {
   const locale = LOCALE.user.dashboard.newTicket;
+  const handleEditorChange = (content) => {
+    console.log('Content was updated:', content);
+  };
+
   return (
-    <>
-      <Box>
+    <Container maxWidth="md">
+      <Box m="0 0 1.5rem">
         <Typography variant="h5" component="h1" align="center">
           {locale.headline}
         </Typography>
       </Box>
-      Create New Ticket page
-    </>
+      <Editor
+        apiKey={process.env.REACT_APP_TINYMCE_KEY}
+        initialValue="<p>This is the initial content of the editor</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [],
+          toolbar: 'undo redo | formatselect | bold italic underline | alignleft aligncenter'
+            + ' alignright alignjustify | bullist numlist | removeformat',
+        }}
+        onEditorChange={handleEditorChange}
+      />
+    </Container>
   );
 };
 
--- src/components/user/dashboard/TicketInfo/MessageForm/MessageForm.jsx
+++ src/components/user/dashboard/TicketInfo/MessageForm/MessageForm.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import { TextField, Typography, Grid, Button, Box } from '@material-ui/core';
 import { LOCALE } from '../../../../../locale';
 
 const MessageForm = () => {
   const locale = LOCALE.user.dashboard.ticketInfo;
+
+  const handleEditorChange = (content) => {
+    console.log('Content was updated:', content);
+  };
 
   return (
     <Box m="0 0 1.5rem">
@@ -19,13 +24,20 @@ const MessageForm = () => {
         }}
         margin="normal"
       />
-      <TextField
-        variant="outlined"
-        margin="normal"
-        fullWidth
-        multiline
-        rows={15}
-      />
+      <Box m="0 0 1rem">
+        <Editor
+          apiKey={process.env.REACT_APP_TINYMCE_KEY}
+          initialValue="<p>This is the initial content of the editor</p>"
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [],
+            toolbar: 'undo redo | formatselect | bold italic underline | alignleft aligncenter'
+              + ' alignright alignjustify | bullist numlist | removeformat',
+          }}
+          onEditorChange={handleEditorChange}
+        />
+      </Box>
       <Grid container justify="flex-end">
         <Button color="primary" variant="contained">
           {locale.form.submitButton}
