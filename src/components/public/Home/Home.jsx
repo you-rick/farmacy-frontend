08dbc40deaa6d06aad50fb0614f3f5b75cddc1d5
imaximciuc <maximciuc.iurii@gmail.com>
2020-08-27 00:20:11 +0300

    redirect on login fix

--- src/components/public/Home/Home.jsx
+++ src/components/public/Home/Home.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { USER_LOGIN_ROUTE, USER_TICKETS_ROUTE } from '../../../routes';
+import { USER_LOGIN_ROUTE, USER_TICKETS_ROUTE, ADMIN_DASHBOARD_ROUTE } from '../../../routes';
 
-const Home = ({ isAuth }) => {
-  const redirectTo = (isAuth) => (isAuth ? USER_TICKETS_ROUTE : USER_LOGIN_ROUTE);
+const Home = ({ isAuth, role }) => {
+  const rolePath = role === 'user' ? USER_TICKETS_ROUTE : ADMIN_DASHBOARD_ROUTE;
+  const redirectTo = (isAuth) => (isAuth ? rolePath : USER_LOGIN_ROUTE);
 
   return <Redirect to={redirectTo(isAuth)} />;
 };
 
 const mapStateToProps = (state) => ({
   isAuth: state.auth.isAuth,
+  role: state.auth.role,
 });
 export default connect(mapStateToProps, {})(Home);
