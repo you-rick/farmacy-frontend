a7ac9774a2f0d9b332d7fc789fe751e8daf7adb1
imaximciuc <maximciuc.iurii@gmail.com>
2020-09-04 18:26:30 +0300

    fix lint

--- src/store/authReducer.js
+++ src/store/authReducer.js
@@ -6,7 +6,7 @@ import { hideNote, setNote } from './notificationReducer';
 import { setToken, removeToken } from '../utils/helpers/token-handler';
 import { setTicketsData } from './ticketsReducer';
 import { setAdminData } from './adminReducer';
-import { USER_LOGIN_ROUTE, ADMIN_LOGIN_ROUTE, ADMIN_USERS_ROUTE } from '../routes';
+import { USER_LOGIN_ROUTE, ADMIN_LOGIN_ROUTE } from '../routes';
 import { serverErrorHelper } from '../utils/helpers/server-error-helper';
 import { LOCALE } from '../locale';
 
