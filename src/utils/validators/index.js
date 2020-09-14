95b26f9d30e51885bce89ebc3584342189c7a4db
imaximciuc <maximciuc.iurii@gmail.com>
2020-09-14 15:40:32 +0300

    uuid validation regex update

--- src/utils/validators/index.js
+++ src/utils/validators/index.js
@@ -1,4 +1,4 @@
 export const EMAIL_VALIDATOR = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-export const TICKET_NUMBER_VALIDATOR = /^[0-9a-z]+$/;
+export const TICKET_NUMBER_VALIDATOR = /[0-9a-fA-F]{8}[0-9a-fA-F]{4}[0-9a-fA-F]{4}[0-9a-fA-F]{4}[0-9a-fA-F]{12}/;
 export const PHONE_NUMBER_PATTERN = '(999) 999-9999';
 export const DATE_FORMAT = 'DD/MM/YYYY';
