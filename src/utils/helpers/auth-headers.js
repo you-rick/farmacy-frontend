d1077c4937fe8c4cfd528a594266f75877925187
imaximciuc <maximciuc.iurii@gmail.com>
2020-12-18 18:03:23 +0200

    small fix

--- src/utils/helpers/auth-headers.js
+++ src/utils/helpers/auth-headers.js
@@ -1,10 +1,7 @@
-// @Todo add localStorage verification
-
 export const authHeaders = (data) => {
   if (!!data.username && !!data.password) {
     return {
-      username: data.username,
-      password: data.password,
+      ...data,
     };
   }
   return {};
